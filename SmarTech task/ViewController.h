4111e2761a7992b7dc3df7b64b66526526ee0631
islam mohamed metwally <metwally@islams-macbook-pro.local>
2016-03-30 18:18:21 +0200

    Initial Commit

---
+++ SmarTech task/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SmarTech task
+//
+//  Created by islam metwally on 3/30/16.
+//  Copyright © 2016 Islam Metwally. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
